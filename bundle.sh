0e90f6496b813b7054d4dfb6e0b0115bb9380534
Sean Krail <sean.krail@jitterbit.com>
2020-03-27 10:55:13 -0700

    ci: replace yml2json with yq v3

--- bundle.sh
+++ bundle.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 set -eo pipefail
 
-echo "Bundling all of the charts..."
-
-# Convert the definitions YAML-based JSON schema into JSON
-yml2json definitions.schema.yaml --output definitions.schema.json
+echo -n "Bundling all of the charts..."
 
 # Loop through every chart
 for chart in charts/*; do
-  # Convert the values YAML-based JSON schema into JSON
-  yml2json "${chart}"/values.schema.yaml --output "${chart}"/values-temp.schema.json
-
-  # Combine the values.schema.json and definitions.schema.json files
-  # with the values.schema.json file taking precedence recursively
-  # See https://stedolan.github.io/jq/manual/#Multiplication,division,modulo:*,/,and%
-  jq --compact-output --slurp '.[0] * .[1]' definitions.schema.json "${chart}"/values-temp.schema.json > "${chart}"/values.schema.json
-
-  # Remove the temporary JSON schema file
-  rm "${chart}"/values-temp.schema.json
+  # Combine the definitions.schema.yaml and charts/$chart/values.schema.yaml files
+  # with the charts/$chart/values.schema.yaml file taking precedence
+  # and output the merged YAML document as JSON.
+  # See https://mikefarah.gitbook.io/yq/commands/merge
+  # and https://mikefarah.gitbook.io/yq/usage/convert
+  yq merge --overwrite --tojson definitions.schema.yaml "${chart}"/values.schema.yaml > "${chart}"/values.schema.json
 done
+
+echo "done"
