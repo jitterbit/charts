e9e604d322f8f258203cbb521867896a3deec907
Jesus G <jesus.gomez@jitterbit.com>
2021-03-25 16:16:02 -0400

    chore(workflow) yq v4 compatible

--- bundle.sh
+++ bundle.sh
@@ -10,7 +10,15 @@ for chart in charts/*; do
   # and output the merged YAML document as JSON.
   # See https://mikefarah.gitbook.io/yq/commands/merge
   # and https://mikefarah.gitbook.io/yq/usage/convert
-  yq merge --overwrite --tojson definitions.schema.yaml "${chart}"/values.schema.yaml > "${chart}"/values.schema.json
+  JSON_SCHEMA="${chart}/values.schema.json"
+  YAML_SCHEMA="${chart}/values.schema.yaml"
+  if [ ! -f "$JSON_SCHEMA" ] 
+  then
+    echo "File $JSON_SCHEMA does not exist"
+    touch "$JSON_SCHEMA"
+  fi
+  yq eval-all --tojson "select(fileIndex == 0) * select(filename == \"$YAML_SCHEMA\")" definitions.schema.yaml "${chart}"/values.schema.yaml > "$JSON_SCHEMA"
+
 done
 
 echo "done"
